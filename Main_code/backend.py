7e4cfd950535014bd497aaead8654c23deef972d
Vaas <geniatullin.marat@gmail.com>
2021-12-19 22:22:40 +0300

    Добавит функцию compare index

--- Main_code/backend.py
+++ Main_code/backend.py
@@ -30,9 +30,15 @@ def choice_user(count: int) -> int:
                 print('Incorrect choice')
     return list_choice
 
-#def compare_index(list_1: list, list_2: list) -> list:
+def compare_index(list_1: list, list_2: list) -> list:
+    count = 0
+    for i in range(len(list_1)):
+        if list_1[i] == list_2[i]:
+            count += 1
+    return count
+
 
 #print(random_colors(8, colors))
 
-x = choice_user(2)
-print(x)
+#x = choice_user(2)
+print(len(colors))
